2c57aa2a36cf80359fb3cdba22f66ac918bad18d
metaadsspider <yashublitzz@gmail.com>
2025-08-10 13:54:23 +0530

    Update fancodeService.ts

--- src/services/fancodeService.ts
+++ src/services/fancodeService.ts
@@ -82,7 +82,7 @@ export class FancodeService {
         buttonColor: "red",
         sportIcon: "🏏",
         status: "live",
-        streamUrl: "api/mumbai/133329_english_hls_93256ta-di_h264/index.m3u8"
+        streamUrl: "/api/mumbai/132763_english_hls_7178ta-di_h264/index.m3u8"
       },
       {
         id: "London Spirit Women VS Welsh Fire Women",
